921ec94cec9c5cadde2007b009bc336baba201a3
Daniel John <dj71286@gmail.com>
2025-09-29 14:59:40 +0100

    Add new solution

---
+++ 0x02main.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+print(f"Hi, my name is {name}, I am {age} years old, and I love {hobby}!")

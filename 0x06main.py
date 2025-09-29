db8f9e4d73f9b30977d46db59e63229f2dc97726
Daniel John <dj71286@gmail.com>
2025-09-29 18:42:18 +0100

    Add new solution

---
+++ 0x06main.py
@@ -0,0 +1,11 @@
+# Step 1: Set the age
+age = 45
+# Step 2: Determine ticket price using conditional statements
+if age < 13:
+    print("Ticket price: $5")
+elif age < 18:
+    print("Ticket price: $7")
+elif age < 65:
+    print("Ticket price: $10")
+else:
+    print("Ticket price: $6")
